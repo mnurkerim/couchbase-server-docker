0a6b6a1864106233e51a230befde9c5e5cc0ae9d
Mo Nurkerim <muhammednurkerim@gmail.com>
2017-11-11 12:23:37 +0000

    Used 127.0.0.1 instead of localhost

--- configure-node.sh
+++ configure-node.sh
@@ -7,7 +7,7 @@ set -m
 sleep 10
 
 couchbase-cli cluster-init -c 127.0.0.1 --cluster-username Administrator --cluster-password password --services data,index,query --cluster-ramsize 1024 --cluster-index-ramsize 512
-couchbase-cli bucket-create -c localhost:8091 -u Administrator -p password --bucket=housekeepingtitles --bucket-type=couchbase --bucket-port=11222 --bucket-ramsize=200 --bucket-replica=1
+couchbase-cli bucket-create -c 127.0.0.1:8091 -u Administrator -p password --bucket=housekeepingtitles --bucket-type=couchbase --bucket-port=11222 --bucket-ramsize=200 --bucket-replica=1
 couchbase-cli user-manage -c 127.0.0.1:8091 -u Administrator -p password --set --rbac-username housekeepingtitles --rbac-password password --rbac-name "housekeepingtitles" --roles bucket_admin[housekeepingtitles] --auth-domain local
 cbq -u housekeepingtitles -p password -e http://localhost:8093 -s="CREATE PRIMARY INDEX ON housekeepingtitles"
 
